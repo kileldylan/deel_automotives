a8763d0aef25ec12a61b1e77b6fa5913d790bffb
Deelan Kilel <kileldylan@gmail.com>
2025-02-20 07:19:52 +0300

    added the call now button and review stars to home cards

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,100 +1,33 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import {
-  Typography,
-  Container,
-  Grid,
-  Box,
-  Card,
-  CircularProgress,
-  CardContent,
-  CardMedia,
-  TextField,
-  Button,
-  IconButton,
-} from '@mui/material';
+import React, { useState } from 'react';
+import { Container, Box, TextField, IconButton, Typography, Button, Grid, Card, CardMedia, CardContent } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
-import { createClient } from '@supabase/supabase-js';
-import CustomAppBar from './customAppbar';
+import StarIcon from '@mui/icons-material/Star';
 import Footer from './footer';
-import carHeroImage from '../images/porsche_banner.jpg';
-import { Link } from 'react-router-dom';
-
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+import CustomAppBar from './customAppbar';
 
 const Home = () => {
   const [searchQuery, setSearchQuery] = useState('');
-  const [carData, setCarData] = useState([]);
-  const [filteredCars, setFilteredCars] = useState([]);
   const [priceFilter, setPriceFilter] = useState('');
-  const [loading, setLoading] = useState(true);
-  const [displayCount, setDisplayCount] = useState(12);
-  const availableCarsRef = useRef(null);
-  const navigate = useNavigate();
-  const fetchCarData = async () => {
-    try {
-      const { data, error } = await supabase.from('cars').select('*');
-      if (error) throw error;
-      setCarData(data);
-      setFilteredCars(data);
-      setDisplayCount(12);
-    } catch (error) {
-      console.error('Error fetching car data:', error.message);
-    } finally {
-      setLoading(false);
-    }
+  const [filteredCars, setFilteredCars] = useState([]);
+  const [displayCount, setDisplayCount] = useState(6);
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    // Implement search logic here
   };
-
-  useEffect(() => {
-    fetchCarData();
-    const intervalId = setInterval(fetchCarData, 60000);
-    return () => clearInterval(intervalId);
-  }, []);
-
-  useEffect(() => {
-    let filtered = carData;
-    if (priceFilter) {
-      filtered = filtered.filter(car => {
-        switch (priceFilter) {
-          case '0-500K': return car.price <= 500000;
-          case '500K-1M': return car.price > 500000 && car.price <= 1000000;
-          case '1M-2M': return car.price > 1000000 && car.price <= 2000000;
-          case '2M-3M': return car.price > 2000000 && car.price <= 3000000;
-          case '3M-5M': return car.price > 3000000 && car.price <= 5000000;
-          case '5M-10M': return car.price > 5000000 && car.price <= 10000000;
-          case '10M+': return car.price > 10000000;
-          default: return true;
-        }
-      });
-    }
-    if (searchQuery) {
-      filtered = filtered.filter(car => car.make.toLowerCase().includes(searchQuery.toLowerCase()));
-    }
-    setFilteredCars(filtered);
-  }, [priceFilter, searchQuery, carData]);
 
   const handleLoadMore = () => {
-    setDisplayCount(prevCount => prevCount + 12);
+    setDisplayCount(displayCount + 6);
   };
 
-  const scrollToAvailableCars = () => {
-    if (availableCarsRef.current) {
-      availableCarsRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
-  
   const handleCardClick = (id) => {
     navigate(`/carRoutes/cars/${id}`);
   };
 
-  const handleSearchSubmit = (event) => {
-    event.preventDefault();
+  const handleCallNow = (phoneNumber) => {
+    window.location.href = `tel:${phoneNumber}`;
   };
 
-  if (loading) return <CircularProgress />;
-  
   return (
     <>
       <CustomAppBar />
@@ -117,7 +50,6 @@ const Home = () => {
         </Typography>
       </Box>
 
-      {/* Hero Section */}
       <Box sx={{ position: 'relative', height: '400px', overflow: 'hidden', width: '100vw', marginLeft: 'calc(-50vw + 50%)' }}>
         <img
           src={carHeroImage}
@@ -167,37 +99,35 @@ const Home = () => {
         </Box>
       </Box>
 
-      {/* Search and Filter */}
       <Container sx={{ marginTop: 3, textAlign: 'center' }}>
         <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-         <form onSubmit={handleSearchSubmit}>
+          <form onSubmit={handleSearchSubmit}>
             <TextField
-                variant="outlined"
-                placeholder="Search for by car name..."
-                sx={{
-                    width: {
-                        xs: '100%', // Full width on extra small screens
-                        sm: '400px', // 400px width on small screens
-                        md: '600px', // 600px width on medium screens and above
-                    },
-                    marginRight: { xs: 0, sm: 1 }, // No margin on extra small screens
-                    marginBottom: { xs: 2, sm: 0 }, // Add margin bottom on extra small screens
-                }}
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)}
-                InputProps={{
-                    endAdornment: (
-                        <IconButton type="submit">
-                            <SearchIcon />
-                        </IconButton>
-                    ),
-                }}
+              variant="outlined"
+              placeholder="Search for by car name..."
+              sx={{
+                width: {
+                  xs: '100%', // Full width on extra small screens
+                  sm: '400px', // 400px width on small screens
+                  md: '600px', // 600px width on medium screens and above
+                },
+                marginRight: { xs: 0, sm: 1 }, // No margin on extra small screens
+                marginBottom: { xs: 2, sm: 0 }, // Add margin bottom on extra small screens
+              }}
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              InputProps={{
+                endAdornment: (
+                  <IconButton type="submit">
+                    <SearchIcon />
+                  </IconButton>
+                ),
+              }}
             />
           </form>
         </Box>
       </Container>
 
-      {/* Price Range Filter */}
       <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexWrap: 'wrap', gap: 1, mt: 1 }}>
         <Typography variant="h6" sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 1, mt: -2 }}>
           Price Range
@@ -212,20 +142,34 @@ const Home = () => {
           </Button>
         ))}
       </Box>
-        
+
       <Container sx={{ mt: 3 }}>
         <Grid container spacing={4}>
           {filteredCars.slice(0, displayCount).map((car) => (
             <Grid item xs={12} sm={6} md={4} key={car.id}>
-              <Card sx={{ width: 300, cursor: 'pointer' }} onClick={() => handleCardClick(car.id)}>                <CardMedia
+              <Card sx={{ width: '100%', cursor: 'pointer' }} onClick={() => handleCardClick(car.id)}>
+                <CardMedia
                   component="img"
                   height="140"
-                  image={car.image_url} 
+                  image={car.image_url}
                   alt={car.model}
                 />
                 <CardContent>
                   <Typography variant="h7">{car.make} {car.model} {car.description}</Typography>
                   <Typography variant="h6" color="text.secondary">Ksh {car.price.toLocaleString()}</Typography>
+                  <Box sx={{ display: 'flex', alignItems: 'center', mt: 1 }}>
+                    {[...Array(5)].map((_, index) => (
+                      <StarIcon key={index} sx={{ color: index < car.rating ? '#ffb400' : '#e0e0e0' }} />
+                    ))}
+                  </Box>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    sx={{ mt: 2 }}
+                    onClick={() => handleCallNow('+254758715788')} // Replace with actual contact number
+                  >
+                    Call Now
+                  </Button>
                 </CardContent>
               </Card>
             </Grid>
